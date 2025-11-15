9bba37e4e0356f2d0fd9897c495492384c008d4c
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-11-15 12:46:30 +0530

    Change database password to a placeholder
    Updated database connection password for security.

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "mother",        
+  password: "#######";     
   database: "instagram_db",
 });
 
